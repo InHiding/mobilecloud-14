7627852399e5eb948d2100babc909a6fc40fac9e
Giuliano Melo <gcrm@ig.com.br>
2014-08-10 23:46:15 -0300

    refactoring controller

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/controller/VideoSvcController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/controller/VideoSvcController.java
@@ -1,8 +1,8 @@
 package org.magnum.dataup.controller;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,51 +15,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import retrofit.client.Response;
-import retrofit.http.GET;
 import retrofit.mime.TypedFile;
 
 @Controller
-public class VideoSvcController implements VideoSvcApi {
-	 private List<Video> videos = new ArrayList<Video>();
-	 private AtomicLong id = new AtomicLong(0L);
+public class VideoSvcController {
+	private Map<Long,Video> videos = new HashMap<Long, Video>();
+	private static final AtomicLong id = new AtomicLong(0L);
 	 
-	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.GET)
-	@GET(VIDEO_SVC_PATH)
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList() {
-		return videos;
+		return videos.values();
 	}
 
-	@RequestMapping(value=VIDEO_SVC_PATH, method=RequestMethod.POST)
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.POST)
 	public @ResponseBody Video addVideo(@RequestBody Video v, HttpServletRequest request) {
 		v.setId(getNextId(1));
-		v.setDataUrl(request.getRequestURL() + "/" + String.valueOf(v.getId()) + "/data");
-		videos.add(v);
+		v.setDataUrl(getDataUrl(v.getId(), request));
+		videos.put(v.getId(), v);
 		return v;
 	}
 
-	@Override
-	public VideoStatus setVideoData(long id, TypedFile videoData) {
+	public VideoStatus setVideoData(long id, MultipartFile videoData) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Response getData(long id) {
+	public void getData(long id) {
 		// TODO Auto-generated method stub
-		return null;
 	}
-
+	
 	// Simple ID Generator 
 	private long getNextId(int granularity) {
 		return id.getAndAdd(granularity);
 	}
-
-	@Override
-	public Video addVideo(Video v) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
+	private String getDataUrl(long videoId, HttpServletRequest request){
+        String url = request.getRequestURL() + "/" + String.valueOf(videoId) + "/data";
+        return url;
+    }
 }
