1d146c714140822410ccf09a6bd21bd9b22ad239
Giuliano Melo <gcrm@ig.com.br>
2014-08-11 21:42:18 -0300

    implementing the last action

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/controller/VideoSvcController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/controller/VideoSvcController.java
@@ -1,19 +1,27 @@
 package org.magnum.dataup.controller;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.magnum.dataup.VideoFileManager;
+import org.magnum.dataup.VideoNotFoundException;
 import org.magnum.dataup.VideoSvcApi;
 import org.magnum.dataup.model.Video;
 import org.magnum.dataup.model.VideoStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,7 +31,13 @@ import retrofit.mime.TypedFile;
 public class VideoSvcController {
 	private Map<Long,Video> videos = new HashMap<Long, Video>();
 	private static final AtomicLong id = new AtomicLong(0L);
-	 
+	private VideoFileManager videoDataMgr;
+	
+	@PostConstruct
+	public void init() throws IOException {
+		videoDataMgr = VideoFileManager.get();
+	}
+	
 	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList() {
 		return videos.values();
@@ -37,13 +51,22 @@ public class VideoSvcController {
 		return v;
 	}
 
-	public VideoStatus setVideoData(long id, MultipartFile videoData) {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public @ResponseBody VideoStatus setVideoData(@PathVariable("id") long id, @RequestParam("data") MultipartFile videoData) throws IOException {
+		Video video = videos.get(id);
+		if (video == null) throw new VideoNotFoundException("Cannot findo video with id:" +id);
+		
+		videoDataMgr.saveVideoData(video, videoData.getInputStream());
+		
+		return new VideoStatus(VideoStatus.VideoState.READY);
 	}
 
-	public void getData(long id) {
-		// TODO Auto-generated method stub
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.GET)
+	public void getData(@PathVariable("id") long id, HttpServletResponse response) throws IOException {
+		Video video = videos.get(id);
+		if (video == null) throw new VideoNotFoundException("Cannot findo video with id:" +id);
+		
+		videoDataMgr.copyVideoData(video, response.getOutputStream());
 	}
 	
 	// Simple ID Generator 
